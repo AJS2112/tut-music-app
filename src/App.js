34986d2ef18be14ee69bcc53a9c10381b3452fc4
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-26 19:21:14 -0300

    add-funcionalidade

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Player from './components/Player';
 
 function App() {
@@ -26,12 +26,23 @@ function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex])
 
   return (
     <div className="App">
       <Player
-        song={songs[currentSongIndex]}
-        nextSong={songs[nextSongIndex]}
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
       />
     </div>
   );
